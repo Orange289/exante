e7a9b2351e88acb64afbca5b753306224c259e87
Anna <apelsann@ya.ru>
2018-09-19 22:52:36 +0300

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+var SETTINGS = {
+	activeClass: 'is-active',
+	hiddenClass: 'is-hidden'
+};
+
+var headerMenu = document.querySelector('.header__menu');
+var headerLink = headerMenu.querySelectorAll('.header__link');
+var headerLinkLength = headerLink.length;
+
+var onLinkCLick = function (evt) {
+
+	for (var i = 0; i < headerLinkLength; i++) {
+		headerLink[i].classList.remove(SETTINGS.activeClass);
+	};
+
+	if (evt.target.classList.contains('header__link')) {
+		evt.target.classList.add(SETTINGS.activeClass);
+	}
+
+};
+
+headerMenu.addEventListener('click', onLinkCLick);
+
+var toggleMenu = document.querySelector('#toggle');
+var homePage = document.querySelector('.home');
+var contentPage = document.querySelector('.page');
+
+var onToggleClick = function () {
+	homePage.classList.toggle(SETTINGS.hiddenClass);
+	contentPage.classList.toggle(SETTINGS.hiddenClass);
+};
+
+toggleMenu.addEventListener('click', onToggleClick);
+
+
+
